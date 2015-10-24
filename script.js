b88fd90263f09da016462bad834f4c29f754ec52
David McGeen <dlmcgeen@charter.net>
2015-10-24 11:56:53 -0400

    making progress

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-	for (var i = 1; i <= 16; i++) {
-		$('<div id="dots"></div>').appendTo('#pad');
+	var numDots = 16;
+	var padWidth = $('#pad').css('width');
+	padWidth = padWidth.slice(0,(padWidth.length)-2);
+	var padUseable = padWidth-((numDots+1)*2);
+	var dotSize = Math.floor(padUseable/numDots);
+	for (var x = 1; x <= numDots; x++) {
+		var row = ("row"+x);
+		$('<div id='+row+'></div>').appendTo('#pad');
+		for (var i = 1; i <= numDots; i++) {
+			$("#row"+x).append('<div class="dots"></div>');
+		};
 	};
+	$('.dots').width(dotSize+'px');
+	$('.dots').height(dotSize+'px');
+	$('.dots').hover(function() {
+		$(this).css("background-color", "black");
+	})
+	
 });
