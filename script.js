0d94a8e07ccf6077f3d9af9aa06fc6b26ffdbfe3
David McGeen <dlmcgeen@charter.net>
2015-10-28 15:56:15 -0400

    the final form

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-	var numDots = 16;
+	var gridSize = 32;
 	var padWidth = $('#pad').css('width');
 	padWidth = padWidth.slice(0,(padWidth.length)-2);
-	var padUseable = padWidth-((numDots+1)*2);
-	var dotSize = Math.floor(padUseable/numDots);
+	var padUseable = padWidth-((gridSize+1)*2);
+	drawGrid(gridSize, padUseable);
+	$('#button').click(function() {
+		gridSize = $('input[name=gridSize]').val();
+		drawGrid(gridSize, padUseable);
+	});
+});
+
+var drawGrid = function (numDots, padSize) {
+	$('.dots').parent().remove();
+
+	var dotSize = Math.floor(padSize/numDots);
 	for (var x = 1; x <= numDots; x++) {
 		var row = ("row"+x);
 		$('<div id='+row+'></div>').appendTo('#pad');
@@ -15,6 +25,5 @@ $(document).ready(function() {
 	$('.dots').height(dotSize+'px');
 	$('.dots').hover(function() {
 		$(this).css("background-color", "black");
-	})
-	
-});
+	});
+}
